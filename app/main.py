9f534ee870a857a7390f3faa56e0003ebb207b50
PRATIK <pratikpatil0307@gmail.com>
2025-12-31 12:09:09 +0530

    add GET /articles/{id} endpoint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,34 @@
 from fastapi import FastAPI
 from app.database import db
 from app.models import ArticleCreate
+from bson import ObjectId
+from bson.errors import InvalidId
+from fastapi import HTTPException
 
 app = FastAPI()
 
+@app.get("/articles")
+def get_articles():
+    articles = []
+    for article in db.articles.find():
+        article["_id"] = str(article["_id"]) 
+        articles.append(article)
+    return articles
+
+@app.get("/articles/{article_id}")
+def get_article_by_id(article_id: str):
+    try:
+        object_id = ObjectId(article_id)
+    except InvalidId:
+        raise HTTPException(status_code=400, detail="Invalid article ID format")
+
+    article = db.articles.find_one({"_id": object_id})
+
+    if not article:
+        raise HTTPException(status_code=404, detail="Article not found")
+
+    article["_id"] = str(article["_id"])
+    return article
 
 
 @app.post("/articles")
